a57b9e3ac5d46bc266bdcc8dafb84002b194fa4e
YJP5920 <yujipei@stu.cqut.edu.cn>
2023-05-21 00:49:02 +0800

    Initial commit

---
+++ hello/hello/HelloBaseVisitor.java
@@ -0,0 +1,22 @@
+// Generated from F:/Projects/JAVA IDEA Project/_20230520_antlr_learn\Hello.g4 by ANTLR 4.12.0
+package hello;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link HelloVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class HelloBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements HelloVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitR(HelloParser.RContext ctx) { return visitChildren(ctx); }
+}
---
+++ hello/hello/HelloListener.java
@@ -0,0 +1,20 @@
+// Generated from F:/Projects/JAVA IDEA Project/_20230520_antlr_learn\Hello.g4 by ANTLR 4.12.0
+package hello;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link HelloParser}.
+ */
+public interface HelloListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link HelloParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void enterR(HelloParser.RContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HelloParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void exitR(HelloParser.RContext ctx);
+}
